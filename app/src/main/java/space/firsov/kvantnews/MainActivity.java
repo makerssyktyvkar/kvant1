eb9326b3cead043590c48f8da985b066fc6b8b28
Firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2020-02-21 17:40:23 +0300

    2

--- app/src/main/java/space/firsov/kvantnews/MainActivity.java
+++ app/src/main/java/space/firsov/kvantnews/MainActivity.java
@@ -14,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView today_text = findViewById(R.id.today);
-        int tp = Integer.valueOf(Objects.requireNonNull(getIntent().getStringExtra("type")));
-        if(tp==1){
+        String login = getIntent().getStringExtra("login");
+        today_text.setText(login);
+        int type = getIntent().getIntExtra("type",0);
+        if(type==1){
             today_text.setText("Вы вошли как администратор");
-        }else if(tp==2){
+        }else if(type==2){
             today_text.setText("Вы вошли как ученик");
-        }else if(tp==3){
+        }else if(type==3){
             today_text.setText("Вы вошли как родитель");
         }else{
             today_text.setText("Вы вошли как учитель");
