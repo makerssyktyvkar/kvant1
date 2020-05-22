2b959b757179d32c4a94b1a7c936cf9ec133cdc5
kropio <60886598+kropio@users.noreply.github.com>
2020-05-23 00:20:29 +0300

    exitup

--- app/src/main/java/space/firsov/kvantnews/ui/exit/ExitFragment.java
+++ app/src/main/java/space/firsov/kvantnews/ui/exit/ExitFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import space.firsov.kvantnews.ui.achievements.AchievementsDB;
 import space.firsov.kvantnews.ui.posts.CoursesNewsOfUserDB;
@@ -23,22 +24,12 @@ public class ExitFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_achievements, container, false);
-        CoursesNewsOfUserDB coursesNewsOfUserDB = new CoursesNewsOfUserDB(getContext());
-        coursesNewsOfUserDB.deleteAll();
-        SupportsDB supportsDB = new SupportsDB(getContext());
-        supportsDB.deleteAll();
-        TimetableDB timetableDB = new TimetableDB(getContext());
-        timetableDB.deleteAll();
-        AchievementsDB achievementsDB = new AchievementsDB(getContext());
-        achievementsDB.deleteAll();
-        User user = new User(getContext());
-        user.deleteAll();
-        startLoginActivity();
+        FragmentManager manager = getActivity().getSupportFragmentManager();
+        ExitDialogFragment exitDialogFragment = new ExitDialogFragment();
+        exitDialogFragment.show(manager, "myDialog");
+
         return root;
     }
-    private void startLoginActivity(){
-        Intent intent = new Intent(getContext(), LoginActivity.class);
-        intent.putExtra("type",1);
-        startActivity(intent);
-    }
+
+
 }
